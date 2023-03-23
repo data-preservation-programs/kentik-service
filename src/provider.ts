afa71637e6f215d6920849a0a55a6124ece1955e
Xinan Xu <39632914+xinaxu@users.noreply.github.com>
2023-03-23 16:05:02 -0700

    xinaxu/get-provider-protocols

---
+++ src/provider.ts
@@ -0,0 +1,65 @@
+import { LotusRPC } from '@filecoin-shipyard/lotus-client-rpc';
+// @ts-ignore
+import { NodejsProvider } from '@filecoin-shipyard/lotus-client-provider-nodejs';
+// @ts-ignore
+import { mainnet } from '@filecoin-shipyard/lotus-client-schema';
+import { Libp2p } from 'libp2p';
+import logger from './logger.js';
+import { peerIdFromString } from '@libp2p/peer-id';
+import { Multiaddr, multiaddr } from '@multiformats/multiaddr';
+import { PeerId } from '@libp2p/interface-peer-id';
+import { abortableSource } from 'abortable-iterator';
+import { decode } from '@ipld/dag-cbor';
+import itconcat from 'it-concat';
+
+export interface ProviderInfo {
+  providerId: string;
+  peerId: PeerId;
+  multiAddrs: Multiaddr[];
+}
+
+interface ProviderProtocolRaw {
+  Name: 'libp2p' | 'bitswap' | 'http',
+  Addresses: Uint8Array[]
+}
+
+export interface ProviderProtocol {
+  name: 'libp2p' | 'bitswap' | 'http',
+  addresses: Multiaddr[]
+}
+
+export class ProviderUtil {
+  public static async GetAllProviders (url = 'https://api.node.glif.io/', token: string | undefined = undefined): Promise<ProviderInfo[]> {
+    const nodejsProvider = new NodejsProvider(url, { token });
+    const lotusClient = new LotusRPC(nodejsProvider, { schema: mainnet.fullNode });
+    logger.info('Calling stateListMiners');
+    const providerIds = await lotusClient.stateListMiners([]);
+    logger.info(`Retrieved ${providerIds.length} providers`);
+    const result : ProviderInfo[] = [];
+    for (const providerId of providerIds) {
+      logger.info({ providerId }, 'Calling stateMinerInfo');
+      const providerInfo = await lotusClient.stateMinerInfo(providerId, []);
+      result.push({
+        providerId,
+        peerId: peerIdFromString(providerInfo.PeerId),
+        multiAddrs: providerInfo.Multiaddrs.map(multiaddr)
+      });
+    }
+    return result;
+  }
+
+  public static async GetProtocols (node: Libp2p, peerId: PeerId, multiAddrs: Multiaddr[]) : Promise<ProviderProtocol[]> {
+    await node.peerStore.addressBook.set(peerId, multiAddrs);
+    logger.info({ peerId: peerId.toString() }, 'Dialing peer');
+    const stream = await node.dialProtocol(peerId, '/fil/retrieval/transports/1.0.0',
+      { signal: AbortSignal.timeout(5000) });
+    const source = abortableSource(stream.source, AbortSignal.timeout(5000));
+    logger.info({ peerId: peerId.toString() }, 'Reading stream');
+    const content = (await itconcat(source)).subarray();
+    const decoded: {Protocols: ProviderProtocolRaw[]} = decode(content);
+    return decoded.Protocols.map(d => ({
+      name: d.Name,
+      addresses: d.Addresses.map(a => multiaddr(a))
+    }));
+  }
+}
---
+++ tests/provider.spec.ts
@@ -0,0 +1,27 @@
+import { peerIdFromString } from '@libp2p/peer-id';
+import { multiaddr } from '@multiformats/multiaddr';
+import { ProviderUtil } from '../src/provider.js';
+import { createLibp2p } from 'libp2p';
+import { tcp } from '@libp2p/tcp';
+import { yamux } from '@chainsafe/libp2p-yamux';
+import { noise } from '@chainsafe/libp2p-noise';
+
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 600_000;
+describe('provider', () => {
+  describe('GetProtocols', () => {
+    it('should return protocols', async () => {
+      const node = await createLibp2p({
+        transports: [tcp()],
+        streamMuxers: [yamux()],
+        connectionEncryption: [noise()]
+      });
+      const peerId = peerIdFromString('12D3KooWG3tqzR9Do6QYyaeAmNtQL2TFv9vNjoMywMwVp9EyUqtR');
+      const multiAddr = multiaddr('/dns4/sp.techgreedy.net/tcp/14003');
+      const protocols = await ProviderUtil.GetProtocols(node, peerId, [multiAddr]);
+      expect(protocols[0].name).toEqual('libp2p');
+      expect(protocols[0].addresses[0].toString()).toEqual('/dns4/sp.techgreedy.net/tcp/14003');
+      expect(protocols[1].name).toEqual('bitswap');
+      expect(protocols[1].addresses[0].toString()).toEqual('/ip4/50.47.9.10/tcp/14005/p2p/12D3KooWJehcf8Gxh4zWuEvRg35WdNcfetzFLT5Yxn4dgG1jqYzL');
+    })
+  })
+})
